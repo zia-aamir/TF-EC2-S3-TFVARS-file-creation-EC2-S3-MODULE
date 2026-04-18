74ab4e62f04b28edb3e0d3abf07649773811f7a3
Shah Shanediwan <aamirshanediwan@gmail.com>
2026-04-18 18:27:02 +0530

    Update main1.tf

--- main1.tf
+++ main1.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "zia" {
+  ami           = var.ami_value
+  instance_type = var.instance_type_value
+}
+
+resource "aws_instance" "saru" {
+  ami           = var.ami_value
+  instance_type = var.instance_type_value
+}
+
+resource "aws_s3_bucket" "aleena" {
+    bucket = var.aws_s3_bucket_value  
+}
 
